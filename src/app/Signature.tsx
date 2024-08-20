b2dcc2b17636f458e209067917ef1ae9f48a8bce
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-20 18:03:17 +0300

    Contact box links and styling

--- src/app/Signature.tsx
+++ src/app/Signature.tsx
@@ -9,27 +9,48 @@ const Signature: React.FC = () => {
     <main>
       <div className="ContactBox">
         <div className="ContactBoxIcons">
-          <Image
-            src="/Envelope.png"
-            alt="Envelope Icon"
-            className="contactIcon"
-            width={100}
-            height={100}
-          />
-          <Image
-            src="/GitHub.png"
-            alt="GitHub Icon"
-            className="contactIcon"
-            width={100}
-            height={100}
-          />
-          <Image
-            src="/LinkedIn.png"
-            alt="LinkedIn Icon"
-            className="contactIcon"
-            width={100}
-            height={100}
-          />
+          <a
+            className="ContactBoxIconContainer"
+            href="mailto:kristopherpepperuk(at)@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/Envelope.png"
+              alt="Envelope Icon"
+              className="contactIcon"
+              width={100}
+              height={100}
+            />
+          </a>
+          <a
+            className="ContactBoxIconContainer"
+            href="https://github.com/KrisHHFI"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/GitHub.png"
+              alt="GitHub Icon"
+              className="contactIcon"
+              width={100}
+              height={100}
+            />
+          </a>
+          <a
+            className="ContactBoxIconContainer"
+            href="https://www.linkedin.com/in/kristopher-pepper-824184136/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/LinkedIn.png"
+              alt="LinkedIn Icon"
+              className="contactIcon"
+              width={100}
+              height={100}
+            />
+          </a>
         </div>
         <div className="ContactBoxText">
           Kristopher Pepper 2024
