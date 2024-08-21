4eeb46d2018b50b87e21c1b072a96803644c5cbc
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-21 08:51:43 +0300

    Github pages stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: isProd ? '/<NextJS-Clock-App>/' : '',
+    images: {
+      unoptimized: true, // Since GitHub Pages doesn't support image optimization
+    },
+  };
+  
+  const isProd = process.env.NODE_ENV === 'production';
+  
+  module.exports = nextConfig;  
 
 export default nextConfig;
