a584330119c00f24c78a3716f7e00874c75d98d4
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-21 09:48:19 +0300

    Github pages stuff 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '../Styling/Styling.css';
+import '../../public/Styling/Styling.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
